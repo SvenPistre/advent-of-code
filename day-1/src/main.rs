80b4cf3754721acac65eac4dc1049834bcd6f4a6
Sven Pistre <sven.pistre@gmail.com>
2024-12-02 01:33:31 +0100

    feat: add crude implementation of day 1 puzzle 2
    Task: Day-1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    first_puzzle()
+    first_puzzle();
+    second_puzzle()
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -48,5 +50,39 @@ fn first_puzzle() -> () {
             .zip(location_id_group2.iter())
             .fold(0, |prev, (id1, id2)| prev + id1.abs_diff(*id2));
         println!("The total distance between both lists is {}", distance)
+    }
+}
+
+fn get_similarity_score(location_ids_group1: Vec<u32>, location_ids_group2: Vec<u32>) -> u32 {
+    let mut occurrence_map = HashMap::new();
+
+    for location_id_group1 in &location_ids_group1 {
+        let occurence_in_group2 = location_ids_group2.iter().fold(0, |prev, location_id| {
+            prev + {
+                if location_id != location_id_group1 {
+                    0
+                } else {
+                    1
+                }
+            }
+        });
+        occurrence_map.insert(location_id_group1, occurence_in_group2);
+    }
+
+    let similarity_score = location_ids_group1.iter().fold(0, |prev, location_id| {
+        prev + location_id * occurrence_map.get(&location_id).unwrap()
+    });
+    similarity_score
+}
+
+fn second_puzzle() -> () {
+    if let Some((location_id_group1, location_id_group2)) =
+        get_location_id_lists_from_file(Path::new("./day-1/inputs/input.txt"))
+    {
+        let similarity_score = get_similarity_score(location_id_group1, location_id_group2);
+        println!(
+            "The total similarity score between both lists is {}",
+            similarity_score
+        )
     }
 }
